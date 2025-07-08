1cf91f67216f8fa05d1c1b775f3fd10688b91305
[turegold] <bacaren21@naver.com>
2025-07-08 17:33:35 +0900

    브랜치 변경 전 커밋

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import router from "./routes/index.js";
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import router from './routes/index.js'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use("/api", router);
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use('/api', router)
 
-export default app;
+export default app
